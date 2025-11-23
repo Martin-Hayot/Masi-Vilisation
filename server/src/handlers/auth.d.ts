ab2cd81350844c1fca8d8fb23ebf552ee49366cf
Martin Hayot <martin.hayot@gmail.com>
2025-11-23 16:55:26 +0100

    Remove build artifacts and update .gitignore
    Co-authored-by: Martin-Hayot <116266177+Martin-Hayot@users.noreply.github.com>

--- server/src/handlers/auth.d.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare const login: (req: Request, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | undefined>;
-export declare const logout: (req: Request, res: Response, next: NextFunction) => void;
-export declare const refresh: (req: Request, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | undefined>;
-//# sourceMappingURL=auth.d.ts.map
--- server/src/handlers/users.d.ts
+++
@@ -1,4 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare const getUsers: (req: Request, res: Response, next: NextFunction) => Promise<void>;
-export declare const createUser: (req: Request, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | undefined>;
-//# sourceMappingURL=users.d.ts.map
